8c96a3b61b7daa93bb4c50cb2dbbe6e6bc97832c
Picooooo <62029534+Picooooo@users.noreply.github.com>
2022-01-22 09:25:49 +0700

    Update hog.py

--- src/method/hog.py
+++ src/method/hog.py
@@ -1,10 +1,10 @@
 from skimage.feature import hog 
+import cv2
 
 class Hog:
     name = "Hog"
 
     def extract(self, img):
-        resized_img = cv2.resize(img, (128, 128))
-        fd, hog_img = hog(resized_img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=True, multichannel=True)
+        fd, hog_img = hog(img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=True, multichannel=True)
     
         return fd
